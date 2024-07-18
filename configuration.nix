ffc1642c390009175802956490daeb3bbb6c966b
salko-ua <chips69jamil69game@gmail.com>
2024-07-18 18:00:48 +0300

    add experimental-features

--- configuration.nix
+++ configuration.nix
@@ -134,4 +134,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
