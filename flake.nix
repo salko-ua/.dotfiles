52d984f836eda2a0368c61c24284a7bf2a69d0ff
salko-ua <chips69jamil69game@gmail.com>
2024-07-21 22:14:07 +0300

    switch to standard template

--- flake.nix
+++ flake.nix
@@ -1,32 +1,54 @@
 {
-  description = "A very basic flake";
+  description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    # Home manager
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:  
-    let
-      nix_lib = nixpkgs.lib;
-      home_lib = home-manager.lib;
-      system = "x86_64-linux";	
-      pkgs = nixpkgs.legacyPackages.${system};
-      outputs = self.outputs;
-    in  {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "x86_64-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    overlays = import ./overlays {inherit inputs;};
+    nixosModules = import ./modules/nixos;
+    homeManagerModules = import ./modules/home-manager;
+
     nixosConfigurations = {
-        nixos = nix_lib.nixosSystem {
-	      inherit system;
-	      specialArgs = {inherit inputs outputs;};
-   	      modules = [ ./configuration.nix ];	
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./nixos/configuration.nix
+        ];
       };
     };
+
+    # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-        salo = home_lib.homeManagerConfiguration {
-	      inherit pkgs;
-	      extraSpecialArgs = {inherit inputs outputs;};
-        modules = [ ./home.nix ];
+      salo = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
+        ];
       };
     };
   };
