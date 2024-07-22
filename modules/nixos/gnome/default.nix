781b58b8c7b57bc0f5f5ecee34df65f2a125c036
salko-ua <chips69jamil69game@gmail.com>
2024-07-22 17:20:15 +0000

    gnome

--- modules/nixos/gnome/default.nix
+++ modules/nixos/gnome/default.nix
@@ -1,5 +1,8 @@
 {
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 }
