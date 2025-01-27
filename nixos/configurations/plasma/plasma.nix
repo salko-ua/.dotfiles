c002bd7ace69475516584cc716819134666c669e
salko-ua <github@salko-ua.de>
2025-01-27 23:50:05 +0200

    switch to wayland <3

--- nixos/configurations/plasma/plasma.nix
+++ nixos/configurations/plasma/plasma.nix
@@ -3,7 +3,7 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession = "plasmax11";
+  services.displayManager.defaultSession = "plasma";
   hardware.pulseaudio.enable = false;
    environment.plasma6.excludePackages = with pkgs.kdePackages; [
      plasma-browser-integration
