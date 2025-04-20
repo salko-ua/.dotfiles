001fb1f779da1a7ee29a57159864c9261f5079be
salko-ua <github@salko-ua.de>
2025-04-20 11:03:36 +0300

    fix vb

--- nixos/configurations/virtualbox/virtualbox.nix
+++ nixos/configurations/virtualbox/virtualbox.nix
@@ -2,6 +2,4 @@
 {
    virtualisation.virtualbox.host.enable = true;
    users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-   virtualisation.virtualbox.host.enable = true;
-   virtualisation.virtualbox.host.enableExtensionPack = true;
 }
