680e65824c7ad90e872f795af3ba8872dea0beb1
salko-ua <github@salko-ua.de>
2026-08-06 12:31:56 +0300

    force variety config and set wallpaper mode to os

--- home-manager/programs/variety/default.nix
+++ home-manager/programs/variety/default.nix
@@ -1,9 +1,16 @@
 {pkgs, ...}: {
   home.packages = [pkgs.variety];
 
+  # variety rewrites both files at runtime, so activation must overwrite them
   xdg.configFile = {
-    "variety/ui.conf".source = ./ui.conf;
-    "variety/variety.conf".source = ./variety.conf;
+    "variety/ui.conf" = {
+      source = ./ui.conf;
+      force = true;
+    };
+    "variety/variety.conf" = {
+      source = ./variety.conf;
+      force = true;
+    };
   };
 
   home.file."Pictures/.keep".text = "Variety needs this";
